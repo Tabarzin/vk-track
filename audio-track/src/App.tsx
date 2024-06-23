0026d362c50217e89b2ffc2b4a938a81234be0c3
Rustam <tabarzin@gmail.com>
2024-06-23 17:36:04 +0300

    refactor: add cursor pointer to dots

--- audio-track/src/App.tsx
+++ audio-track/src/App.tsx
@@ -43,7 +43,7 @@ const App: React.FC = observer(() => {
         <div className="timer">
           <div className="timer">{renderTime()}</div>
         </div>
-        <Icon20MoreVertical fill={"blue"} />
+        <Icon20MoreVertical fill={"blue"} style={{ cursor: "pointer" }} />
       </div>
     </div>
   );
